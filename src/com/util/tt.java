cca08e2189f2570bf7f1a3fbcf0cdfee9bedf9ee
1772793156 <53066156+1772793156@users.noreply.github.com>
2022-09-25 22:44:18 +0800

    Update tt.java
    copy 2

--- src/com/util/tt.java
+++ src/com/util/tt.java
@@ -3,5 +3,10 @@ package com.util;
 public class tt {
     public static void main(String[] args) {
         System.out.println('b');
+        System.out.println('b');
+        System.out.println('b');
+        System.out.println('b');
+        System.out.println('b');
+        System.out.println('b');
     }
 }
