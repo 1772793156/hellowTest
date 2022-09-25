df8df309bb6d6400db3922f40f4f9c427ede3a4d
songyj <syj177279356@123.com>
2022-09-25 22:36:13 +0800

    master third commit

--- src/com/mayikt/HelloWord.java
+++ src/com/mayikt/HelloWord.java
@@ -3,6 +3,7 @@ package com.mayikt;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word");
 
     }
 }
